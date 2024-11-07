804c0001f36f297efcaa0d24a27621d7d9c6a6eb
kakageo <116504211+Saka1zum1@users.noreply.github.com>
2024-11-08 00:35:14 +0800

    Update discord-api.js

--- functions/discord-api.js
+++ functions/discord-api.js
@@ -1,52 +1,40 @@
-let fetch;
-
-async function init() {
-  const { default: fetched } = await import('node-fetch');
-  fetch = fetched;
-}
-
-init();
-
-const cache = {}; 
-
+let lastRequestTime = 0;
 
 exports.handler = async function(event, context) {
+  const { default: fetch } = await import('node-fetch');
+  
   const discordToken = process.env.DISCORD_TOKEN;
   const { channel_id, message_id } = event.queryStringParameters;
+
+ 
+  const currentTime = Date.now();
+  const timeDifference = currentTime - lastRequestTime;
   
+  if (timeDifference < 1000) {
+    return {
+      statusCode: 429, 
+      body: JSON.stringify({ error: 'Rate limit exceeded. Please try again in a few seconds.' })
+    };
+  }
+  
+
+  lastRequestTime = currentTime;
+
   if (!channel_id || !message_id) {
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'Missing channel_id or message_id' })
     };
   }
-
- 
-  const validChannels = ["774703077172838430", "1148013283006218352"];
-  if (!validChannels.includes(channel_id)) {
+  else if (!["774703077172838430", "1148013283006218352"].includes(channel_id)){
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'Wrong channel_id' })
     };
   }
 
-
-  const cacheKey = `${channel_id}-${message_id}`;
-
-
-  if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp) < 15000) {  
-    return {
-      statusCode: 429,
-      body: JSON.stringify({ error: 'Too many requests. Please wait before retrying.' })
-    };
-  }
-
   try {
- 
-    cache[cacheKey] = { timestamp: Date.now() };
-
     const url = `https://discord.com/api/v9/channels/${channel_id}/messages?around=${message_id}&limit=1`;
-
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -63,7 +51,6 @@ exports.handler = async function(event, context) {
     }
 
     const data = await response.json();
-
     if (data.length > 0) {
       const attachments = data[0].attachments;
       const attachmentUrl = attachments[0].url;
@@ -77,7 +64,6 @@ exports.handler = async function(event, context) {
         body: JSON.stringify({ message: 'No attachments found.' })
       };
     }
-
   } catch (error) {
     console.error('Error fetching Discord message:', error);
     return {
