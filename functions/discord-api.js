ff6f6da763b4533191e46f9b48aceceb6d969e34
kakageo <116504211+Saka1zum1@users.noreply.github.com>
2024-11-07 23:03:35 +0800

    Update discord-api.js

--- functions/discord-api.js
+++ functions/discord-api.js
@@ -1,8 +1,11 @@
-const fetch = require('node-fetch'); 
-exports.handler = async function(event,context) {
+exports.handler = async function(event, context) {
+  const { default: fetch } = await import('node-fetch');
+
+  console.log('Fetching attachments for channel:', event.queryStringParameters.channel_id, 'and message:', event.queryStringParameters.message_id);
+
   const discordToken = process.env.DISCORD_TOKEN;
   const { channel_id, message_id } = event.queryStringParameters;
-  console.log('Fetching attachments for channel:', channel_id, 'and message:', message_id)
+  
   if (!channel_id || !message_id) {
     return {
       statusCode: 400,
@@ -11,13 +14,12 @@ exports.handler = async function(event,context) {
   }
 
   try {
-
     const url = `https://discord.com/api/v9/channels/${channel_id}/messages?around=${message_id}&limit=1`;
-    
+
     const response = await fetch(url, {
       method: 'GET',
       headers: {
-        'Authorization': discordToken,
+        'Authorization': `Bot ${discordToken}`,
         'Content-Type': 'application/json'
       }
     });
@@ -31,10 +33,9 @@ exports.handler = async function(event,context) {
 
     const data = await response.json();
 
-
     if (data.length > 0) {
       const attachments = data[0].attachments;
-      const attachmentUrl = attachments[0].url
+      const attachmentUrl = attachments[0].url;
       return {
         statusCode: 200,
         body: JSON.stringify({ attachmentUrl })
@@ -45,7 +46,7 @@ exports.handler = async function(event,context) {
         body: JSON.stringify({ message: 'No attachments found.' })
       };
     }
-    
+
   } catch (error) {
     console.error('Error fetching Discord message:', error);
     return {
