2d628387e57280bc6f274e4dc650b3ee60d6b1a4
kakageo <116504211+Saka1zum1@users.noreply.github.com>
2024-11-07 20:33:37 +0800

    Create discord-api.js

---
+++ functions/discord-api.js
@@ -0,0 +1,56 @@
+const fetch = require('node-fetch'); 
+exports.handler = async function(event) {
+  const discordToken = process.env.DISCORD_TOKEN;
+  const { channel_id, message_id } = event.queryStringParameters;
+  
+  if (!channel_id || !message_id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Missing channel_id or message_id' })
+    };
+  }
+
+  try {
+
+    const url = `https://discord.com/api/v9/channels/${channel_id}/messages?around=${message_id}&limit=1`;
+    
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': discordToken,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: `Failed to fetch message. Status: ${response.status}` })
+      };
+    }
+
+    const data = await response.json();
+
+
+    if (data.length > 0) {
+      const attachments = data[0].attachments || [];
+      const attachmentUrls = attachments.map(attachment => attachment.url);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ attachmentUrls })
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'No attachments found.' })
+      };
+    }
+    
+  } catch (error) {
+    console.error('Error fetching Discord message:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal server error' })
+    };
+  }
+};
