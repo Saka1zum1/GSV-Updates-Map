749056bb51fdcc4b0c893ee08553cedb8c89a661
kakageo <1556025485@qq.com>
2025-10-03 14:33:32 +0800

    bug fixes

--- functions/checkUpdateType.js
+++ functions/checkUpdateType.js
@@ -18,21 +18,30 @@ exports.handler = async function (event, context) {
             body: JSON.stringify({ error: "Missing required parameters: country, year, month" }),
         };
     }
-    if (!channel_id) channel_id = '747031995770470460'
+
     try {
         const connection = await mysql.createConnection(connectionConfig);
 
-        const [rows] = await connection.execute(`
+        const [rows] = await connection.execute(channel_id?`
             SELECT
                 MAX(CASE WHEN country = ? AND region = ? AND year = ? AND month = ? AND channel_id = ? THEN 1 ELSE 0 END) AS has_region,
                 MAX(CASE WHEN country = ? AND year = ? AND month = ? AND channel_id = ? THEN 1 ELSE 0 END) AS has_country,
                 MAX(CASE WHEN region = ? AND year = ? AND channel_id = ? THEN 1 ELSE 0 END) AS has_year
-                FROM update_reports`,
-            [
-                country, region, year, month, channel_id,
-                country, year, month, channel_id,
-                region, year, channel_id
-            ]);
+                FROM update_reports`:`
+            SELECT
+                MAX(CASE WHEN country = ? AND region = ? AND year = ? AND month = ? THEN 1 ELSE 0 END) AS has_region,
+                MAX(CASE WHEN country = ? AND year = ? AND month = ? THEN 1 ELSE 0 END) AS has_country,
+                MAX(CASE WHEN region = ? AND year = ? THEN 1 ELSE 0 END) AS has_year
+            FROM update_reports
+        `, channel_id?[
+            country, region, year, month,
+            country, year, month,
+            region, year,channel_id
+        ]:[
+            country, region, year, month,
+            country, year, month,
+            region, year
+        ]);
 
         await connection.end();
 
